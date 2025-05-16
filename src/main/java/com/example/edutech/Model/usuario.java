1559f5e2a765772340d00646f2b7da7fbf1d15ad
TremorSongg <tremorsong86@gmail.com>
2025-05-16 04:00:04 -0400

    Usuario
    Creacion de modelo Usuario

---
+++ src/main/java/com/example/edutech/Model/usuario.java
@@ -0,0 +1,23 @@
+package com.example.edutech.Model;
+
+import java.util.Optional;
+
+import jakarta.persistence.*;
+import lombok.Data;;
+
+@Entity
+@Data
+
+public class usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String email;
+    private String password;
+
+    public static Optional<usuario> map(Object o){
+        throw new UnsupportedOperationException("Unimplemented method 'map'");
+    }
+}
