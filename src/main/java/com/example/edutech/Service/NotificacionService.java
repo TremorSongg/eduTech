33d332f90cb6c4c20da9c5e6d1806f0226557935
olovj222 <ol.juengt@duocuc.cl>
2025-05-22 10:28:16 -0400

    Se añade NotificacionService pasos iniciales

---
+++ src/main/java/com/example/edutech/Service/NotificacionService.java
@@ -0,0 +1,15 @@
+package com.example.edutech.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.edutech.Repository.NotificacionRepository;
+
+@Service
+public class NotificacionService {
+    
+    @Autowired
+    private NotificacionRepository notificacionRepository;
+    
+    //public List<Notificacion> 
+}
