fb3ba354ae375d08e0a9cf91ce74299b2c2fb196
olovj222 <ol.juengt@duocuc.cl>
2025-05-24 17:11:07 -0400

    Update ReporteIncidenciaController.java

--- src/main/java/com/example/edutech/Controller/ReporteIncidenciaController.java
+++ src/main/java/com/example/edutech/Controller/ReporteIncidenciaController.java
@@ -3,6 +3,7 @@ package com.example.edutech.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.edutech.Model.EstadoSolicitud;
 import com.example.edutech.Model.ReporteIncidencia;
 import com.example.edutech.Service.ReporteIncidenciaService;
 
@@ -10,6 +11,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import java.util.Map;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/api/v1/reportes")
@@ -27,12 +31,31 @@ public class ReporteIncidenciaController {
             int usuarioId = Integer.parseInt(datos.get("usuarioId"));
             String mensaje = datos.get("mensaje");
 
-            ReporteIncidencia nuevaSolicitud = reporteIncidenciaService.crearSolicitud(usuarioId, mensaje);
+            ReporteIncidencia solicitud = new ReporteIncidencia();
+            solicitud.setUsuarioId(usuarioId);
+            solicitud.setMensaje(mensaje);
+            solicitud.setEstado(EstadoSolicitud.PENDIENTE);
+
+            ReporteIncidencia nuevaSolicitud = reporteIncidenciaService.crearSolicitud(solicitud);
             return ResponseEntity.ok(nuevaSolicitud);
         } catch (Exception e) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.badRequest().body(null);
         }
     }
     
-    
+    //Cambiar estado de solicitud y enviar notificacion
+    @PutMapping("/estado/{id}")
+    public ResponseEntity<ReporteIncidencia> cambiarEstado(
+            @PathVariable("id") int id,
+            @RequestBody Map<String, String> datos){
+        try{
+            EstadoSolicitud nuevoEstado = EstadoSolicitud.valueOf(datos.get("estado"));
+            ReporteIncidencia actualizada = reporteIncidenciaService.cambiarEstado(id, nuevoEstado);
+            return ResponseEntity.ok(actualizada);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+
+        }
+
 }
