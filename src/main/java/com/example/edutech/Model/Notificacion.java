be91f00602049dd4285ff6593635f8e6cead0b5f
olovj222 <ol.juengt@duocuc.cl>
2025-05-24 17:43:49 -0400

    Se añaden archivos de componente de microservicio Notificaciones

--- src/main/java/com/example/edutech/Model/Notificacion.java
+++ src/main/java/com/example/edutech/Model/Notificacion.java
@@ -1,23 +1,25 @@
-// package com.example.edutech.Model;
+package com.example.edutech.Model;
 
 
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Id;
-// import lombok.AllArgsConstructor;
-// import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
-// @AllArgsConstructor 
-// @NoArgsConstructor 
-// @Entity
-// public class Notificacion {
-//     @Id
-//     @GeneratedValue(strategy =GenerationType.IDENTITY)
-//     private int id;
-//     private int usuarioId;
-//     private String mensaje; 
+@AllArgsConstructor 
+@NoArgsConstructor 
+@Entity
+@Data
+public class Notificacion {
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    private int id;
+    private int usuarioId;
+    private String mensaje; 
 
 
-// }
+}
