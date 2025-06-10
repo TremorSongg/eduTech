2dbf2d115d842f01a76c6289e14cca9e7ad6211e
olovj222 <ol.juengt@duocuc.cl>
2025-06-10 11:03:09 -0400

    2 tests restantes

---
+++ src/test/java/com/example/edutech/Controller/CarritoControllerIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.example.edutech.Controller;
+
+import com.example.edutech.Service.CursoService;
+import com.example.edutech.Model.Curso;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+//@WebMvcTest(CursoController.class)
+public class CarritoControllerIntegrationTest {
+    
+}
